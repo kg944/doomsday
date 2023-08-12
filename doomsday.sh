4c611698dd78562826a7d4b00a593e6aea7f5553
Killian Greene <kuabapps@gmail.com>
2023-08-12 16:42:33 -0400

    Add doomsday shell script

--- doomsday.sh
+++ doomsday.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-echo "Doomsday"
+#!/bin/zsh
+# TODO figure out how to get dates before 1971 and after 2038
+random_date_epoch=$(shuf -n1 -i$(date -d '1971-01-01' '+%s')-$(date -d '2037-12-31' '+%s') )
+date_str=$(date -d @$randome_date_epoch '+%B %d, %Y')
+weekday=$(date -d @$random_date_epoch '+%A')
+
+echo $date_str"?"
+read answer
+
+echo $weekday
